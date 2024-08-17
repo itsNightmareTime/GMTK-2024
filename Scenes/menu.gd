000f22a26f17ac2d021845468eb219494c2b871d
nightmarefails <harmstrong@armsoft.dev>
2024-08-17 03:25:14 -0400

    feature: main menu with music

---
+++ Scenes/menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file('res://Scenes/game.tscn')
+
+func _on_options_button_pressed():
+	pass # TODO: implement
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file('res://Scenes/credits.tscn')
+
+func _on_quit_button_pressed():
+	get_tree().quit()
