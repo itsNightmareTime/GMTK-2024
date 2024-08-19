ee9798d442fd2f03a7169ed096da38956aa5294d
itsBrianTime <superbrian69@gmail.com>
2024-08-19 14:33:23 -0700

    Feature:
    Timer added to the game. needs a game manager to have it fully function still

---
+++ Scripts/timer.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@export var start_seconds: int = 30
+@export var start_minutes: int = 1
+var current_seconds: int
+var current_minutes: int
+@onready var timer_text: Label = $Label
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	Reset_Timer()
+	timer.start()
+
+func Reset_Timer():
+	current_seconds = start_seconds
+	current_minutes = start_minutes
+	set_text()
+
+func set_text():
+	timer_text.text = str(current_minutes) + ":" + str(current_seconds).pad_zeros(2)
+
+
+func _on_timer_timeout() -> void:
+	if current_seconds == 0:
+		if current_minutes > 0:
+			current_minutes -= 1
+			current_seconds = 60
+	else:
+		current_seconds -= 1
+	set_text()
