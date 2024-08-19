2a2713c04cad57f0f158f63cb81fc7f7d8f0d549
itsBrianTime <superbrian69@gmail.com>
2024-08-19 11:53:29 -0700

    Bug Fix:
    Spent 2 hours trying to fix pushing and it turned out to be a small issue of a collision mask not set on raycast2d. I want to die...

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,17 +5,17 @@ enum PLAYER_NUM {
 	TWO = 2,
 	THREE = 3
 }
-const SPEED = 10000.0
-const RUN_SPEED = SPEED * 1.5
-const BLOCK_SPEED = 0.005
+const SPEED = 15000.0
+const BLOCK_PULL_SPEED: float = 0.75
+const BLOCK_PUSH_SCALAR: float = 2.75
+const BLOCK_FRICTION: float = 0.3 #determines how long it takes to push/pull before the block starts moving
 var block_contact_time: float = 0.0
 @export var PLAYER_NUMBER: PLAYER_NUM = PLAYER_NUM.ONE
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var detect_interaction: RayCast2D = $Detect_Interaction
+@onready var my_detect_interaction: RayCast2D = get_node(PLAYER_NAMES[PLAYER_NUMBER] + "RayCast2D")
 
-
-var last_direction = "Down"
+var last_direction = "Right"
 var detect_interaction_length: int = 1
 var detect_interaction_direction: Vector2 = Vector2(0,1)
 
@@ -27,39 +27,34 @@ const PLAYER_NAMES = {
 
 func _process(delta):
 	
-	
-	push_block(delta)
-	
-	# can switch between running and walking speeds
 	var current_speed = SPEED
 	velocity = Vector2.ZERO
 	
+	push_block(delta)
+	
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Interact"):
 		Interaction()
-	
-	#if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER + "Run"):
-	current_speed = RUN_SPEED
 
 	#Get Action
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Up"):
 		velocity.y -= current_speed
 		last_direction = "Up"
-		detect_interaction.target_position = Vector2(0, -detect_interaction_length)
+		my_detect_interaction.target_position = Vector2(0, -detect_interaction_length)
 		
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Down"):
 		velocity.y += current_speed
 		last_direction = "Down"
-		detect_interaction.target_position = Vector2(0, detect_interaction_length)
+		my_detect_interaction.target_position = Vector2(0, detect_interaction_length)
 		
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Right"):
 		velocity.x += current_speed
 		last_direction = "Right"
-		detect_interaction.target_position = Vector2(detect_interaction_length, 0)
+		my_detect_interaction.target_position = Vector2(detect_interaction_length, 0)
 		
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Left"):
 		velocity.x -= current_speed
 		last_direction = "Left"
-		detect_interaction.target_position = Vector2(-detect_interaction_length, 0)
+		my_detect_interaction.target_position = Vector2(-detect_interaction_length, 0)
 
 	# Handle opposite directions
 	if Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Up") and Input.is_action_pressed(PLAYER_NAMES[PLAYER_NUMBER] + "Down"):
@@ -77,24 +72,23 @@ func _process(delta):
 	move_and_slide()
 	
 func push_block(delta):
-	# Check for block contact
-	if detect_interaction.is_colliding() and not no_input():
+	# Check for block contact with constant pushing
+	if my_detect_interaction.is_colliding() and not no_input():
 		block_contact_time += delta
+		# once push time exceeds block friction, it can start moving
+		if block_contact_time >= BLOCK_FRICTION:
+			var block = my_detect_interaction.get_collider()
+			if block and block is RigidBody2D:
+				var the_block: RigidBody2D = block
+				the_block.move_and_collide(Direction_To_Vector2D()/BLOCK_PUSH_SCALAR)
+	# Player is either not touching a block or not holding a direction towards it
 	else:
 		block_contact_time = 0.0
 		
-	if block_contact_time >= 0.2:
-		var block = detect_interaction.get_collider()
-		if block and block is RigidBody2D:
-			#var push_force = velocity.normalized() * BLOCK_SPEED
-			#block.apply_central_impulse(push_force)
-			var the_block: RigidBody2D = block
-			the_block.move_and_collide(Direction_To_Vector2D()/2)
-		
 func Interaction():
-	if detect_interaction.is_colliding():
+	if my_detect_interaction.is_colliding():
 		#print("touchy touch touch")
-		var the_object = detect_interaction.get_collider()
+		var the_object = my_detect_interaction.get_collider()
 		print("the object we touched was: " + the_object.name)
 	else:
 		print("no touchy")
